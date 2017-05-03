a749061d21247e5c57487eab76a72af9a24f276f
Duc Pham <minhducpham.it@gmail.com>
2017-05-03 07:54:01 +0700

    add

--- js/common.js
+++ js/common.js
@@ -15,5 +15,37 @@
 		}, 'slow');
 	});
 
+	// Slide Carousel
+  	$(document).ready(function() {
+	    $(".owl-carousel").each(function(index, el) {
+	      var config = $(this).data();
+	      config.navText = ['<i class="fa fa-angle-left"></i>','<i class="fa fa-angle-right"></i>'];
+	      config.smartSpeed="800";
+	      if($(this).hasClass('owl-style2')){
+	        config.animateOut="fadeOut";
+	        config.animateIn="fadeIn";    
+	      }
+	      if($(this).hasClass('dotsData')){
+	        config.dotsData="true";
+	      }
+	      $(this).owlCarousel(config);
+	    });
+	});
+
+	/** Menu, Menu Mega Responsive **/
+	$(document).ready(function(){
+		$('.menu>ul>li.parent').append('<span class="plus"></span>');
+		$('.menu ul li.parent .plus').click(function(){
+			$(this).toggleClass('open').siblings('.submenu').slideToggle();
+		});
+	});
+	/*  [ Main Menu ]
+  - - - - - - - - - - - - - - - - - - - - */
+  $(".navbar-toggle").on( 'click', function() {
+    $(this).toggleClass('has-open');
+    $("#main-menu .menu").toggleClass("has-open");
+    $("body").toggleClass("menu-open");
+  });
+
 
 })(jQuery); // End of use strict
