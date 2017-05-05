91564507fab1416b076a9c21057a1cadf8715986
Ducpm <minhducpham.it@gmail.com>
2017-05-05 17:51:57 +0700

    Duc

--- js/common.js
+++ js/common.js
@@ -61,5 +61,61 @@
     /*  [ Sticky Menu ] */
   	$('.fix-header ').sticky({ topSpacing: 0 });
 
-
 })(jQuery); // End of use strict
+
+// Adv scroll top
+$(document).ready(function() {
+    if ($("#advLeft").show(), $("#advRight").show(), l = ".banner_scroll .item", l.length > 0) {
+        var r = 1e3,
+            o = $("#advLeft .banner_scroll").width(),
+            a = $("#advRight .banner_scroll").width(),
+            v = ($(document).width() - r) / 2 + r,
+            e = ($(document).width() - r) / 2 - o;
+        $(window).scroll(function() {
+            h()
+        });
+        $(window).resize(function() {
+            h()
+        });
+
+        function h() {
+            if ($(document.body).width() < r + o + a) {
+                $(".banner_scroll").css("display", "none");
+                return
+            }
+            $(".banner_scroll").css("display", "block");
+            v = ($(document.body).width() - 0 - r) / 2 + r + 10;
+            e = o == null ? ($(document.body).width() - 0 - r) / 2 - a - 10 : ($(document.body).width() - 0 - r) / 2 - o - 10;
+            var f = 0,
+                n = $(window).scrollTop(),
+                u = window.location.pathname != "/" ? 160 : 460,
+                t = 0,
+                i = $(".footer").position().top - $(".banner_scroll").height() - 6;
+            t = n < u ? u - n : n >= i ? 246 : 0;
+            $("#advLeft .banner_scroll .item").length != 0 && (n >= i ? $("#advLeft .banner_scroll").css({
+                position: "fixed",
+                bottom: t,
+                top: "",
+                left: e
+            }) : $("#advLeft .banner_scroll").css({
+                position: "fixed",
+                top: t + 43,
+                bottom: "",
+                left: e
+            }));
+            $("#advRight .banner_scroll .item").length != 0 && (n >= i ? $("#advRight .banner_scroll").css({
+                position: "fixed",
+                bottom: t,
+                top: "",
+                right: e
+            }) : $("#advRight .banner_scroll").css({
+                position: "fixed",
+                top: t + 43,
+                bottom: "",
+                right: e
+            }));
+            f = n
+        }
+        h()
+    }
+});
