f8edf0ebbe7713a8c63d841f4e7bad9275a3845f
Ducpm <minhducpham.it@gmail.com>
2017-05-09 15:09:26 +0700

    add

--- js/common.js
+++ js/common.js
@@ -61,6 +61,16 @@
     /*  [ Sticky Menu ] */
   	$('.fix-header ').sticky({ topSpacing: 0 });
 
+    /*  Collapse */
+    $('.collapse.in').prev('.panel-heading').addClass('active');
+        $('#accordion, #bs-collapse')
+        .on('show.bs.collapse', function (a) {
+            $(a.target).prev('.panel-heading').addClass('active');
+        })
+        .on('hide.bs.collapse', function (a) {
+            $(a.target).prev('.panel-heading').removeClass('active');
+        });
+
 })(jQuery); // End of use strict
 
 // Adv scroll top
